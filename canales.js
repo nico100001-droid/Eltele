8cb3823cff0ac58b352a5a16be5e41c5a18a495d
nico100001-droid <nico_100_001@hotmail.com>
2026-02-13 09:51:55 -0300

    Update canales.js

--- canales.js
+++ canales.js
@@ -1,1 +1,17 @@
-
+const listaCanales = [
+{ nombre: "HBO HD", url: "" },
+{ nombre: "PRUEBA", url: "" },
+{ nombre: "CANAL 3", url: "#" },
+{ nombre: "CANAL 4", url: "#" },
+{ nombre: "CANAL 5", url: "#" },
+{ nombre: "CANAL 6", url: "#" },
+{ nombre: "CANAL 7", url: "#" },
+{ nombre: "CANAL 8", url: "#" },
+{ nombre: "CANAL 9", url: "#" },
+{ nombre: "CANAL 10", url: "#" },
+{ nombre: "CANAL 11", url: "#" },
+{ nombre: "CANAL 12", url: "#" },
+{ nombre: "CANAL 13", url: "#" },
+{ nombre: "CANAL 14", url: "#" },
+{ nombre: "CANAL 15", url: "#" }
+];
