4b4b30a28e31f6787ba9538ccc7f142ee4d827d6
nico100001-droid <nico_100_001@hotmail.com>
2026-03-30 01:03:27 -0300

    Update and rename renovadoras.js to Animados.m3u8

--- renovadoras.js
+++
@@ -1,6 +0,0 @@
-// renovadoras.js
-const canalesYoutube = [
-    { nombre: "reno1", url: "https://www.youtube.com/watch?v=yMVHMrIsHyo" },
-    { nombre: "reno2", url: "https://www.youtube.com/watch?v=8WaytmOBqEQ" },
-    { nombre: "reno3", url: "https://www.youtube.com/watch?v=zfSaxnaauAM" },
-];
