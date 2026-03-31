970b27d2e09e81117ae1678420e62454a34de0f6
nico100001-droid <nico_100_001@hotmail.com>
2026-03-31 19:18:54 -0300

    Update DibusNico.js

--- DibusNico.js
+++ DibusNico.js
@@ -1,1 +1,5 @@
+// SimpsonsTV.js
+const listaSimpsons = [
+    { "nombre": "E1 - América", "url": "https://ia800905.us.archive.org/28/items/lossimpsonstemp4/Los%20Simpsons%20temporada%204/Temporada%204/401.mp4" },
 
+  
