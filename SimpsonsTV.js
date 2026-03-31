9841e78368d20d91c5d43a1f57dc189d35b3bbad
nico100001-droid <nico_100_001@hotmail.com>
2026-03-31 10:29:14 -0300

    Create SimpsonsTV.js

---
+++ SimpsonsTV.js
@@ -0,0 +1,7 @@
+// SimpsonsTV.js
+const listaSimpsons = [
+    { "nombre": "E1 - América", "url": "https://ia800905.us.archive.org/28/items/lossimpsonstemp4/Los%20Simpsons%20temporada%204/Temporada%204/401.mp4" },
+    { "nombre": "E2 - Faraón", "url": "https://ia800905.us.archive.org/28/items/lossimpsonstemp4/Los%20Simpsons%20temporada%204/Temporada%204/402.mp4" },
+    { "nombre": "E3 - Loto Azul", "url": "https://ia800905.us.archive.org/28/items/lossimpsonstemp4/Los%20Simpsons%20temporada%204/Temporada%204/403.mp4" }
+    // Agregá todos los que quieras aquí...
+];
