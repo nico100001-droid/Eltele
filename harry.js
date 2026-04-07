d77bb58a76f42b5e3ccf9c906d7c03af290e003f
nico100001-droid <nico_100_001@hotmail.com>
2026-04-07 18:27:02 -0300

    Update and rename simpsonTV.m3u8 to harry.js

---
+++ harry.js
@@ -0,0 +1,10 @@
+// harry.js
+const listaharry = [
+    { "nombre": "01", "url": "" },
+    { "nombre": "02", "url": "" },
+    { "nombre": "03", "url": "" },
+    { "nombre": "el cáliz de fuego 04", "url": "" },
+    { "nombre": "05", "url": "" },
+    { "nombre": "06", "url": "" },
+    { "nombre": "07", "url": "" },
+    { "nombre": "08", "url": "" },
