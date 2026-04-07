15279c2f3dbba774d1e0c8f5c539f4d91e0de39c
nico100001-droid <nico_100_001@hotmail.com>
2026-04-07 18:30:06 -0300

    Update harry.js

--- harry.js
+++ harry.js
@@ -1,9 +1,9 @@
 // harry.js
 const listaharry = [
-    { "nombre": "01", "url": "" },
+    { "nombre": "la piedra filosofal 01", "url": "" },
     { "nombre": "02", "url": "" },
     { "nombre": "03", "url": "" },
-    { "nombre": "el cáliz de fuego 04", "url": "" },
+    { "nombre": "el cáliz de fuego 04", "url": "https://jefferycontrolmodel.com/e/zw3hzw59h7bf" },
     { "nombre": "05", "url": "" },
     { "nombre": "06", "url": "" },
     { "nombre": "07", "url": "" },
