d1dd2ab71a6dcbac9aa9808965801ab7d1a5ead9
nico100001-droid <nico_100_001@hotmail.com>
2026-04-16 00:29:09 -0300

    Update canales.js

--- canales.js
+++ canales.js
@@ -2,6 +2,7 @@ const listaCanales = [
   { nombre: "ESPN",
     fuentes: [
       { url: "https://cointv.site/cvatt.html?get=RVNQTkhE&start=true", tipo: "iframe" },
+      { url: "https://streamtpnew.com/global2.php?stream=tv_publica", tipo: "iframe" },
       { url: "https://streamx550.com/global2.php?channel=espn", tipo: "iframe" }
     ] },
   { nombre: "ESPN2",
@@ -31,15 +32,80 @@ const listaCanales = [
     fuentes: [
       { url: "https://live-04-11-tyc24.vodgc.net/tyc24/index.m3u8", tipo: "hls" }
     ] },
-    { nombre: "HBO HD",
+      { nombre: "DTS",
+    fuentes: [
+      { url: "https://streamtpnew.com/global2.php?stream=dsports2", tipo: "iframe" },
+    ] },
+      { nombre: "DTS +",
+    fuentes: [
+      { url: "https://streamtpnew.com/global2.php?stream=dsportsplus", tipo: "iframe" },
+    ] },
+    { nombre: "HBO",
     fuentes: [
       { url: "https://cointv.site/html/cvatt.html?get=SEJPSEQ=", tipo: "iframe" },
-      { url: "https://respaldo1.com/hbo.m3u8", tipo: "hls" }
     ] },
   { nombre: "CH 15 Cine", 
     fuentes: [
       { url: "https://stmv4.voxtvhd.com.br/canal5pueblo/canal5pueblo/playlist.m3u8", tipo: "hls" },
-      { url: "https://otro-servidor.com/cine.m3u8", tipo: "hls" }
     ] },
-  
+  { nombre: "NEO TV", 
+    fuentes: [
+      { url: "https://videostream.shockmedia.com.ar:19360/neotvdigital/neotvdigital.m3u8", tipo: "hls" },
+    ] },
+  { nombre: "ACM", 
+    fuentes: [
+      { url: "https://amc-amcespanol-1-us.plex.wurl.tv/playlist.m3u8", tipo: "hls" },
+    ] },
+  { nombre: "Cineaccion", 
+    fuentes: [
+      { url: "https://stmv6.voxtvhd.com.br/cineaccion/cineaccion/playlist.m3u8", tipo: "hls" },
+    ] },
+  { nombre: "Villa Maria TV", 
+    fuentes: [
+      { url: "https://vivo.solumedia.com:19360/uniteve/uniteve.m3u8", tipo: "hls" },
+    ] },
+    { nombre: "CH 5 Tigre",
+    fuentes: [
+      { url: "https://videostream.shockmedia.com.ar:19360/cincotv/cincotv.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "Tele Junin",
+    fuentes: [
+      { url: "https://videostream.shockmedia.com.ar:19360/telejunin/telejunin.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "Garaje TV",
+    fuentes: [
+      { url: "https://stream1.sersat.com/hls/garagetv.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "RetroTV",
+    fuentes: [
+      { url: "https://stream.enlineatv.net/hls/retrotv/index.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "Kanade",
+    fuentes: [
+      { url: "https://static.kanade.cl/hls/kanade.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "VISION",
+    fuentes: [
+      { url: "https://cdn.streamhispanatv.net:3531/live/tvsretrogtlive.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "Pitufos",
+    fuentes: [
+      { url: "https://imps-smrfs.otteravision.com/imps/smrfs/smrfs.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "Nickelodeon",
+    fuentes: [
+      { url: "https://jmp2.uk/plu-5d8d08395f39465da6fb3ec4.m3u8", tipo: "hls" },
+    ] },
+    { nombre: "C5N",
+    fuentes: [
+      { url: "https://val04.ride.com.ar/tvmia/an06/playlist.m3u8", tipo: "hls" },
+    ] },
+      { nombre: "DIBUS TV",
+    fuentes: [
+      { url: "ALEATORIO", tipo: "hls" },
+    ] },
+        { nombre: "HARRY",
+    fuentes: [
+      { url: "ALEATORIO", tipo: "hls" },
+    ] },
 ];
