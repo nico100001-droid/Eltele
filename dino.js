0e5905e530b5da596750f89b2a66880711fd8c98
nico100001-droid <nico_100_001@hotmail.com>
2026-04-17 18:23:42 -0300

    Create dino.js

---
+++ dino.js
@@ -0,0 +1,16 @@
+const dino = {
+  "Serie A": {
+    "Temporada 1": [
+      { id: "uQJWcrjC_OY", title: "Episodio 1" },
+      { id: "_4SQ2mWxnEc", title: "Episodio 2" }
+      { id: "tamPoLLiEew", title: "Episodio 3" }
+      { id: "WHzX5T5PjBE", title: "Episodio 4" }
+      { id: "2za5RwplXdI", title: "Episodio 5" }      
+    ],
+    "Temporada 2": [
+      { id: "OH57rtnKCEM", title: "Episodio 1" },
+      { id: "WpA23XqUnq0", title: "Episodio 2" }
+      { id: "Sc-RYjjOtNQ", title: "Episodio 3" }
+    ],
+    }
+};
