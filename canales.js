27c5bf02c2ade90cd8bd2d3462f05b5927e5430a
nico100001-droid <nico_100_001@hotmail.com>
2026-05-05 16:24:13 -0300

    Update canales.js

--- canales.js
+++ canales.js
@@ -153,10 +153,6 @@ const listaCanales = [
     ] },
         { nombre: "HARRY",
     fuentes: [
-      { url: "ALEATORIO", tipo: "hls" },
-    ] },
-          { nombre: "Pagina Anime",
-    fuentes: [
-      { url: "https://ww3.animeonline.ninja/inicio/", tipo: "iframe" },
+      { url: "FUENTES", tipo: "hls" },
     ] },
 ];
