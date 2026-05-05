9bad420ecf3dcb7f61898b03b58a485893ffaf94
nico100001-droid <nico_100_001@hotmail.com>
2026-05-05 15:35:51 -0300

    Update harry.js

--- harry.js
+++ harry.js
@@ -7,5 +7,8 @@ const listaharry = [
     { "nombre": "la orden del Fénix 05", "url": "https://jefferycontrolmodel.com/e/4mu28xmc6t3k" },
     { "nombre": "el misterio del príncipe06", "url": "https://jefferycontrolmodel.com/e/o548916w2kej" },
     { "nombre": "las Reliquias de la Muerte: Parte 1 07", "url": "https://jefferycontrolmodel.com/e/vyj8xlbi18pz" },
-    { "nombre": "las Reliquias de la Muerte: Parte 2 08", "url": "https://jefferycontrolmodel.com/e/pcz1p47xlkb5" },  
+    { "nombre": "las Reliquias de la Muerte: Parte 2 08", "url": "https://jefferycontrolmodel.com/e/pcz1p47xlkb5" },
+    { "nombre": "Animales Fantásticos: Y Dónde Encontrarlos", "url": "https://hgcloud.to/e/s0677c2p5u0h" },
+    { "nombre": "Animales Fantásticos: los crímenes de Grindelwald", "url": "https://uqload.is/embed-hxyxac78reyf.html" },
+    { "nombre": "Animales Fantásticos 3: Los Secretos de Dumbledore", "url": "https://uqload.is/embed-u8q8jtbc6cku.html" },
   ];
