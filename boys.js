337e39a697397fa0727fe3e50f461955ef46dbf1
nico100001-droid <nico_100_001@hotmail.com>
2026-06-18 20:52:50 -0300

    Update boys.js

--- boys.js
+++ boys.js
@@ -1,1 +1,20 @@
-
+const boys = {
+  "Serie A": {
+"Temporada ": [
+      { url: "https://minochinos.com/v/wzg3lpv3kwlj", title: "Episodio 1" },
+      { url: "hhttps://minochinos.com/v/ot9p5oxqvuu7", title: "Episodio 2" },
+      { url: "https://minochinos.com/v/xwr3eotlxhrb", title: "Episodio 3" },
+      { url: "https://minochinos.com/v/oe3wa1iuqkg8", title: "Episodio 4" },
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E05.mp4", title: "Episodio 5" },
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E06.mp4", title: "Episodio 6" },
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E07.mp4", title: "Episodio 7" },
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E08.mp4", title: "Episodio 8" },
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E52.mp4", title: "Episodio 52" }
+    ],
+    "Temporada 2": [
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E01.mp4", title: "Episodio 1" },
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E02.mp4", title: "Episodio 2" },
+      { url: "https://archive.org/download/digimon-adventure-latino/Digimon%20Adventure%20-%20S01E03.mp4", title: "Episodio 3" }
+    ]
+    }
+};
