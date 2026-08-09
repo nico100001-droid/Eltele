077b66930f91acbf9cd0f206f175a2f8e15a4d54
nico100001-droid <nico_100_001@hotmail.com>
2026-08-09 14:30:22 -0300

    Update harry.js

--- harry.js
+++ harry.js
@@ -2,7 +2,7 @@
 const listaharry = [
     { "nombre": "la piedra filosofal 01", "url": "https://uqload.is/embed-xgq9z725f5ud.html" },
     { "nombre": "La Camara Secreta 02", "url": "https://uqload.is/embed-w62yrm45ct9c.html" },
-    { "nombre": "el prisionero de Azkaban 03", "url": "https://uqload.is/embed-jvlfy5m8fwrn.html" },
+    { "nombre": "el prisionero de Azkaban 03", "url": "https://vidsrc-embed.ru/embed/movie?tmdb=673&autoplay=1&ds_lang=es" },
     { "nombre": "el cáliz de fuego 04", "url": "https://uqload.is/embed-f3ziu587jy2q.html" },
     { "nombre": "la orden del Fénix 05", "url": "https://vsembed.ru/embed/movie?tmdb=675&autoplay=1&ds_lang=es" },
     { "nombre": "el misterio del príncipe06", "url": "https://callistanise.com/v/vpi89550p7oy" },
