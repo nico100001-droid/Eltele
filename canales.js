1da044d2a31a1309f18818f5af3132339fbf90a6
nico100001-droid <nico_100_001@hotmail.com>
2026-09-06 09:58:38 -0300

    Update canales.js

--- canales.js
+++ canales.js
@@ -12,7 +12,7 @@ const listaCanales = [
     ] },
   { nombre: "ESPN disney",
     fuentes: [
-      { url: "https://streamxhd.com/live1.php?stream=disney17", tipo: "iframe" },
+      { url: "https://streamxhd.com/live1.php?stream=disney2", tipo: "iframe" },
 ] },
     { nombre: "DAZN F1",
     fuentes: [
